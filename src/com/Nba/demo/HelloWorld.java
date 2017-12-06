8a3a696171387028104115fce34ddcd6dc275cb7
lrving223 <714870662@qq.com>
2017-12-06 10:24:34 +0800

    Initial commit

---
+++ src/com/Nba/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.Nba.demo;
+
+/**
+ * Created by XuFan on 2017/12/5.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("哎呀我去!!!");
+    }
+
+}
